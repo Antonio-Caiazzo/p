5dd24c97297a098af1e0df060c559178540fdd68
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-05-23 15:12:02 +0200

    id-2 resolved

--- src/main/java/control/ProductControl.java
+++ src/main/java/control/ProductControl.java
@@ -23,13 +23,13 @@ import model.ProductModel;
  */
 public class ProductControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	static ProductModel model;
-
+	
 	static {
 		model = new ProductModel();
 	}
-
+	
 	public ProductControl() {
 		super();
 	}
@@ -40,46 +40,48 @@ public class ProductControl extends HttpServlet {
 		if (request.getParameter("action") != null && request.getParameter("action").compareTo("dettaglio") == 0) {
 			String codiceStr = request.getParameter("codice");
 			int codice = Integer.parseInt(codiceStr);
-
+			
 			ProductModel model = new ProductModel();
 			try {
 				ProductBean prodotto = model.doRetrieveByKey(codice);
 				request.setAttribute("prodottoDettaglio", prodotto);
 			} catch (SQLException e) {
-
-				response.sendRedirect("../webapp/errorpage.html");
-			} finally {
+				e.printStackTrace();
+			}
+			finally {
 				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/productDetail.jsp");
 				dispatcher.forward(request, response);
 			}
-		} else if (request.getParameter("action") != null && request.getParameter("action").compareTo("elimina") == 0) {
+		}
+		else if (request.getParameter("action") != null && request.getParameter("action").compareTo("elimina") == 0) {
 			@SuppressWarnings("unchecked")
 			Collection<ProductBean> lista = (Collection<ProductBean>) request.getSession().getAttribute("products");
 			int codice = Integer.parseInt(request.getParameter("codice"));
 			Collection<ProductBean> collezione = model.deleteProduct(codice, lista);
-
+			
 			request.getSession().removeAttribute("products");
 			request.getSession().setAttribute("products", collezione);
-			// response.sendRedirect(request.getContextPath() + "/ProductsPage.jsp");
+			//response.sendRedirect(request.getContextPath() + "/ProductsPage.jsp");
 			request.getSession().setAttribute("refreshProduct", true);
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ProductsPage.jsp");
 			dispatcher.forward(request, response);
-		} else if (request.getParameter("action") != null
-				&& request.getParameter("action").compareTo("modificaForm") == 0) {
+		}
+		else if (request.getParameter("action") != null && request.getParameter("action").compareTo("modificaForm") == 0) {
 			ProductBean bean;
 			try {
 				bean = model.doRetrieveByKey(Integer.parseInt(request.getParameter("codice")));
 				request.setAttribute("updateProd", bean);
 			} catch (NumberFormatException e) {
-				response.sendRedirect("../../webapp/errorpage.html");
+				e.printStackTrace();
 			} catch (SQLException e) {
-				response.sendRedirect("../../webapp/errorpage.html");
-			} finally {
-				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/modifica-prodotto.jsp");
+				e.printStackTrace();
+			}
+			finally {
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/modifica-prodotto.jsp"); 
 				dispatcher.forward(request, response);
 			}
-		} else if (request.getParameter("action") != null
-				&& request.getParameter("action").compareTo("modifica") == 0) {
+		}
+		else if (request.getParameter("action") != null && request.getParameter("action").compareTo("modifica") == 0) {
 			ProductBean bean = new ProductBean();
 			bean.setCodice(Integer.parseInt(request.getParameter("codice")));
 			bean.setNome(request.getParameter("nome"));
@@ -88,38 +90,36 @@ public class ProductControl extends HttpServlet {
 			bean.setSpedizione(Double.parseDouble(request.getParameter("spedizione")));
 			bean.setTag(request.getParameter("tag"));
 			bean.setTipologia(request.getParameter("tipologia"));
-
+			
 			model.updateProduct(bean);
 			if (request.getSession().getAttribute("carrello") != null) {
 				CartModel cartmodel = new CartModel();
-				CartBean newCart = cartmodel.updateCarrello(bean,
-						(CartBean) request.getSession().getAttribute("carrello"));
+				CartBean newCart = cartmodel.updateCarrello(bean, (CartBean) request.getSession().getAttribute("carrello"));
 				request.getSession().setAttribute("carrello", newCart);
 			}
 			if (request.getSession().getAttribute("preferiti") != null) {
 				PreferitiModel preferitiModel = new PreferitiModel();
 				@SuppressWarnings("unchecked")
-				Collection<ProductBean> lista = preferitiModel.updatePreferiti(bean,
-						(Collection<ProductBean>) request.getSession().getAttribute("preferiti"));
+				Collection<ProductBean> lista = preferitiModel.updatePreferiti(bean, (Collection<ProductBean>) request.getSession().getAttribute("preferiti"));
 				request.getSession().setAttribute("preferiti", lista);
 			}
-
+			
 			request.getSession().setAttribute("refreshProduct", true);
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
-			dispatcher.forward(request, response);
-		} else {
-			String tipologia = (String) request.getSession().getAttribute("tipologia");
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp"); 
+			dispatcher.forward(request, response);	
+		}
+		else {
+		String tipologia = (String) request.getSession().getAttribute("tipologia");
 
-			try {
-				request.removeAttribute("products");
-				request.setAttribute("products", model.doRetrieveAll(tipologia));
-			} catch (SQLException e) {
-				response.sendRedirect("../../webapp/errorpage.html");
-			}
+		try { 
+			request.removeAttribute("products");
+			request.setAttribute("products", model.doRetrieveAll(tipologia));
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/ProductsPage.jsp?tipologia=" + tipologia);
-			dispatcher.forward(request, response);
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ProductsPage.jsp?tipologia=" + tipologia);
+		dispatcher.forward(request, response);
 		}
 	}
 
